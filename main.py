93a2c666c30cc108614efdda72987d8b8f2ef81e
Klaus Schussler <random.klaus@gmail.com>
2018-12-14 23:32:19 +0100

    Changes made

--- main.py
+++ main.py
@@ -2,12 +2,45 @@
 
 import gprs
 import machine
+import time
 
-gprs.init_gprs_and_http()
-heater_on = gprs.check_heater_on()
+one_quarter = 900000
+one_minute = 60000
 
-print("heater: " + str(heater_on))
+pin_3g = machine.Pin(15, machine.Pin.OUT, value=1)
+remote_on = machine.Pin(33, machine.Pin.OUT, value=1)
+remote_off = machine.Pin(27, machine.Pin.OUT, value=1)
 
-if heater_on:
-    pin = machine.Pin(13, machine.Pin.OUT)
-    pin.value(1)
+
+def turn_heater_on():
+    remote_on.value(0)
+    time.sleep(2)
+    remote_on.value(1)
+
+
+def turn_heater_off():
+    remote_off.value(0)
+    time.sleep(2)
+    remote_off.value(1)
+
+
+def read_from_3g():
+    pin_3g.value(1)
+    time.sleep(20)
+    gprs.init_gprs_and_http()
+    return gprs.check_heater_on()
+
+
+while True:
+
+    heater_on = read_from_3g()
+    print("heater: " + str(heater_on))
+
+    if heater_on:
+        turn_heater_on()
+    else:
+        turn_heater_off()
+
+    pin_3g.value(0)
+    machine.deepsleep(one_quarter)
+
